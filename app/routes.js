1fd9abea988779d1d4c2fd6948526f22c767488e
Austin Ban <banaustinban@gmail.com>
2018-03-18 13:20:14 -0500

    Reverted change

--- app/routes.js
+++ app/routes.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import { Route, Switch, HashRouter } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Home from './routes/home/Home';
 import About from './routes/about/About';
 
 export default (
-	<HashRouter>
-		<Switch>
-			<Route exact path="/" component={Home} />
-			<Route path="/about" component={About} />
-		</Switch>
-	</HashRouter>
+	<Switch>
+		<Route exact path="/" component={Home} />
+		<Route path="/about" component={About} />
+	</Switch>
 );
