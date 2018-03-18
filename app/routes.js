5b6c0045aaa9a5d544c1a4203aba0d3bb37638e0
Austin Ban <banaustinban@gmail.com>
2018-03-18 13:41:55 -0500

    Added google analytics

--- app/routes.js
+++ app/routes.js
@@ -2,9 +2,15 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './routes/home/Home';
 import About from './routes/about/About';
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-89118704-2');
+
+function fireTracking() {
+    ReactGA.pageview(window.location.hash);
+}
 
 export default (
-	<Switch>
+	<Switch onUpdate={fireTracking}>
 		<Route exact path="/" component={Home} />
 		<Route path="/about" component={About} />
 	</Switch>
